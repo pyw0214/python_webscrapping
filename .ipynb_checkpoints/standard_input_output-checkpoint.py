9a78f244e840f5e29499c7511d299a4313e1d338
pyw0214 <yongwookp@gmail.com>
2021-03-28 07:31:26 +0900

    updated_0328

---
+++ .ipynb_checkpoints/standard_input_output-checkpoint.py
@@ -0,0 +1,18 @@
+# import sys
+# print("Python", "Java", file=sys.stdout)  # standard output
+# print("Python", "Java", file=sys.stderr)  # standard error
+
+
+# scores = {"Math": 0, "Eng": 95, "Coding": 100}
+# for subject, score in scores.items():
+#     #print(subject, score)
+#     print(subject.ljust(8), str(score).rjust(4), sep=":")  # left, right adjust
+
+# waiting number
+# 001, 002, 003, ..
+# for num in range(1, 21):
+#     print("waiting num : " + str(num).zfill(3))
+
+# answer = input("Input anything: ")
+# print(type(answer))
+# print("you input " + answer + " accordingly.")
