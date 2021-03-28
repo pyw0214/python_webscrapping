2b7255b08b2bf3f8938ae5398a45bbb94afeca89
pyw0214 <yongwookp@gmail.com>
2021-03-28 20:55:46 +0900

    updated_0328

--- class.py
+++ class.py
@@ -34,15 +34,28 @@
 class Unit:
     def __init__(self, name, hp, damage):    #__init__: 생성자
         self.name = name
-        self.hp = hp
+        self.hp = hp    #멤버 변수
         self.damage = damage
         print("{0} 유닛이 생성되었습니다.".format(self.name))
         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
 
-marine1 = Unit("마린", 40, 5)
-marine2 = Unit("마린", 40, 5)
-tank = Unit("탱크", 150, 40)
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 40)
+
+#레이스 : 공중 유닛, 비행기. 클로킹 가능
+
+wraith1 = Unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 :{1}".format(wraith1.name, wraith1.damage))
+
+#마인드 컨트롤 : 상대방 유닛을 아군으로 만드는 것.
+
+wraith2 = Unit("레이스", 80, 5)
+wraith2.clocking = True
+
+if wraith2.clocking == True:
+    print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
 
 
 
