1fb62ff3d6d1ff80e77f734c1809b99fae509a3c
pyw0214 <yongwookp@gmail.com>
2021-04-02 20:55:18 +0900

    updated_quiz04_module_0402

---
+++ Module.py
@@ -0,0 +1,27 @@
+# import theater_module
+
+
+# theater_module.price(3) #3명이서 영화보러 갔을 때 가격
+# theater_module.price_morning(4) #네 명이서 조조영화 보러 갔을 때
+# theater_module.price_soldier(5) #군인 다섯이서 영화 보러 갔을 때
+
+# import theater_module as mv
+
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
+
+# from theater_module import *
+# #from random import *   # 주석 달지 않고 그냥 모든 걸 사용하겠다.
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+# from theater_module import price, price_morning
+
+# price(5)
+# price_morning(6)
+
+from theater_module import price_soldier as price
+price(3)
+
